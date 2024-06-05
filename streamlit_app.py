3a02639eb3d22d04046cadaf1930c477d8554b7b
asatoskar7 <144487392+asatoskar7@users.noreply.github.com>
2024-06-05 10:27:58 -0400

    Update streamlit_app.py
    putting json into df

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ from snowflake.snowpark.functions import col
 import requests
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -16,6 +13,7 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
+
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -41,3 +39,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothies is ordered, '+ name_on_order+ '!', icon ="✅")
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True) 
+
